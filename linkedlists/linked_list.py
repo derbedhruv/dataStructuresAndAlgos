8eb16948584ff4ef14a67f8e37ca7dcf2053e10f
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2017-01-02 19:36:28 -0800

    removed redundant methods for Node class

--- linkedlists/linked_list.py
+++ linkedlists/linked_list.py
@@ -1,44 +1,37 @@
 '''
 	SINGLY LINKED LIST IMPLEMENTATION IN PYTHON
 '''
-# create node class
-class node:
+# create Node class
+class Node:
 	def __init__(self, value, next=None):
 		self.value = value
 		self.next = next
 
-	def next(self):
-		return next
+class Linkedlist:
+	def __init__(self, startNode):
+		self.startNode = startNode
 
-	def value(self):
-		return value
-
-
-class linkedlist:
-	def __init__(self, startnode):
-		self.startnode = startnode
-
-	def insert(self, nextnode):
+	def insert(self, nextNode):
 		# keep looping till you reach the end and then insert
-		current_node = self.startnode
-		while(current_node.next != None):
-			current_node = current_node.next
+		current_Node = self.startNode
+		while(current_Node.next != None):
+			current_Node = current_Node.next
 
-		current_node.next = nextnode
+		current_Node.next = nextNode
 
 	def display(self):
-		current_node = self.startnode
-		while(current_node.next != None):
-			print(current_node.value), "->",
-			current_node = current_node.next
+		current_Node = self.startNode
+		while(current_Node.next != None):
+			print(current_Node.value), "->",
+			current_Node = current_Node.next
 		print "NULL"
 
 
-a = node(5)
-a.next = node(7)
-# print a.next.value
+a = Node(5)
+a.next = Node(7)
+print a.next.value
 
 
-b = linkedlist(a)
-b.insert(node(8))
+b = Linkedlist(a)
+b.insert(Node(8))
 b.display()
