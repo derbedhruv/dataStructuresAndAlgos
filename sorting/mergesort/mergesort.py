6fc5322c2b3d4723fe797077e3320e5f04a2dd74
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2017-01-03 14:47:22 -0800

    added merge sort module

---
+++ sorting/mergesort/mergesort.py
@@ -0,0 +1,56 @@
+'''
+	MERGE SORT IMPLEMENTATION IN PYTHON
+'''
+
+def mergesort(array):
+	"""
+	sorts input list "array" recursively using the merge sort routine
+	"""
+
+	def merge(a, b):
+		"""
+		merge subroutine
+		Merges two sorted lists (a and b) together
+		"""
+		def lowest_candidate(p, q):
+			return min((x, i) for i,x in enumerate([p, q]) if x != None)
+
+		def element(x, countx):
+			# returns next element in list and None if we are out of the list
+			if countx < len(x):
+				return x[countx]
+			else:
+				return None
+
+		# increment pointers (counta, countb) to list elements of each sub-list passed to merge()
+		counta = 0
+		countb = 0
+		merged_list = []
+		for _ in range(len(a) + len(b)):
+			candidate, i = lowest_candidate(element(a, counta), element(b, countb))
+			merged_list.append(candidate)
+
+			increment_a, increment_b = [1 if j == i else 0 for j in range(2)]
+			counta += increment_a
+			countb += increment_b
+
+		return merged_list
+
+	l = len(array)
+	# BASE CASE: If the array is two elements or less then sort it in a trivial way
+	if l == 2:
+		return [min(array), max(array)]
+	if l == 1:
+		return array
+
+	# NORMAL CASE: Recursively sort and merge
+	# split the array into two parts
+	first = array[:l/2]
+	second = array[l/2:]
+
+	# then run each through mergesort
+	first_sorted = mergesort(first)
+	second_sorted = mergesort(second)
+
+	# Then merge the two
+	return merge(first_sorted, second_sorted)
