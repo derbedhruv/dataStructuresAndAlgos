fb647bdbbe27270ffeee3e9dbf272c27ad4a8081
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2017-01-02 23:16:39 -0800

    works perfectly - linked list implementation

--- linkedlists/linked_list.py
+++ linkedlists/linked_list.py
@@ -8,30 +8,75 @@ class Node:
 		self.next = next
 
 class Linkedlist:
-	def __init__(self, startNode):
+	def __init__(self, startNode = None):
 		self.startNode = startNode
 
 	def insert(self, nextNode):
 		# keep looping till you reach the end and then insert
 		current_Node = self.startNode
-		while(current_Node.next != None):
-			current_Node = current_Node.next
+		if (current_Node != None):
+			while(current_Node.next != None):
+				current_Node = current_Node.next
 
-		current_Node.next = nextNode
+			current_Node.next = nextNode
+		else:
+			self.startNode = nextNode
 
 	def display(self):
 		current_Node = self.startNode
-		while(current_Node.next != None):
+		if (current_Node == None):
+			return
+
+		while(current_Node != None):
 			print(current_Node.value), "->",
 			current_Node = current_Node.next
 		print "NULL"
 
 
-a = Node(5)
-a.next = Node(7)
-print a.next.value
+## Interview practice question: will code up a linkedlist which has two types of elements and then use the "runner" approach to change the nodes to alternate between each type
+count = 2
+type1 = [0 for _ in range(count)]
+type2 = ['x' for _ in range(count)]
+
+L = Linkedlist()
+
+# insert first type of element
+for t1 in type1:
+	L.insert(Node(t1))
+
+for t2 in type2:
+	L.insert(Node(t2))
+
+print "First linked list:"
+L.display()
+
+# Now we will apply the runner method
+pointer1 = L.startNode
+pointer2 = L.startNode
+
+while(pointer2 != None):
+	pointer1 = pointer1.next
+
+	for _ in range(2):
+		pointer2 = pointer2.next
+
+# Now the second pointer is at the end and the first one is at the middle, now re-shuffle
+# Move pointer2 to the start again, and advance pointer1 to the first node of the second type
+# and do a pass through the whole linked list
+pointer2 = L.startNode
+
+while(True):
+	temp = pointer2.next
+	pointer2.next = pointer1
+	pointer2 = temp
+
+	temp = pointer1
+	pointer1 = pointer1.next
+	if (pointer1 == None):
+		temp.next = None
+		break
+	temp.next = pointer2
+
+L.display()
 
 
-b = Linkedlist(a)
-b.insert(Node(8))
-b.display()
