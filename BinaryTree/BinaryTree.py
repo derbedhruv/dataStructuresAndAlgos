7bd8ba091565a5f70e17f45f50909c8e5cdeb1e0
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2017-08-31 21:03:07 -0700

    Initiatlize binary tree with list

--- BinaryTree/BinaryTree.py
+++ BinaryTree/BinaryTree.py
@@ -6,7 +6,7 @@ The following methods have been implemented:
 
 AUTHOR: Dhruv Joshi
 """
-import sys
+import sys, math
 sys.path.append("../Node")
 
 from Node import Node
@@ -20,20 +20,16 @@ class BinaryTreeNode(Node):
     self.right = right
 
 class BinaryTree:
-  def __init__(self, rootVal=None):
-    # initialize a binary tree
-    # point to the root node
-    if rootVal == None:
-      self.root = None
-    else:
-      self.root = BinaryTreeNode(rootVal)
-
-  def createFromList(self, L):
+  def __init__(self, L):
     # create a binary tree from an
     # array representation of the same
     # An element of None will represent a null node
     # 'n' is the index of the element on the list
     # i.e. our current 'root' is the n'th element (1-indexed)
+    # first convert the list to length 2^n by appending the 
+    # requisite number of None elements to the end
+    L += [None]*(len(L) - 2**int(math.log(len(L), 2)))
+
     def createFromListInternal(L, n=1):
       if 2*n+1 >= len(L):
         return None
@@ -43,3 +39,21 @@ class BinaryTree:
       return root
 
     self.root = createFromListInternal(L)
+    self.numElements = len(L)
+
+  def display(self):
+    # pretty print the binary tree on terminal
+    # do not print NULL values
+    printStack = []   # stack for printing, keep appending to this
+
+    # convert all the keys to string (if not None)
+    # and pad with spaces to make them of the same length (== max(all lengths))
+
+
+    def displayLine(lineNum):
+      # print a single line of nodes
+      # print space if there's a NULL node
+      pass
+
+
+
